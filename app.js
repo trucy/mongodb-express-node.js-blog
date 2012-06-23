522db820a608757899379f613f5dbb81f5aaea92
Trucy Phan <trucy.phan@gmail.com>
2012-06-23 15:44:25 -0400

    adding all files

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
+
+var app = module.exports = express.createServer();
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+var articleProvider= new ArticleProvider();
+
+app.get('/', function(req, res){
+    articleProvider.findAll( function(error,docs){
+        res.render('index.jade', { locals: {
+            title: 'a simple blog',
+            articles:docs
+            }
+        });
+    })
+});
+
+app.listen(3000);
