38af10d7372f09a01a930d7e1da79094aea230d7
Trucy Phan <trucy.phan@gmail.com>
2012-06-26 11:30:59 -0400

    added ability to connect to mongodb and leave comments, updated stylesheets

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+/**
+ * Module dependencies.
+ */
+
 var express = require('express');
-var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
+var ArticleProvider = require('./articleprovider-mongodb').ArticleProvider;
+
 
 var app = module.exports = express.createServer();
+
+// Configuration
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
@@ -14,20 +21,22 @@ app.configure(function(){
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler());
+  app.use(express.errorHandler()); 
 });
 
-var articleProvider= new ArticleProvider();
+var articleProvider = new ArticleProvider('localhost', 27017);
+// Routes
 
 app.get('/', function(req, res){
     articleProvider.findAll( function(error,docs){
-        res.render('index.jade', { locals: {
-            title: 'a simple blog',
-            articles:docs
+        res.render('index.jade', { 
+            locals: {
+                title: 'a simple blog',
+                articles:docs
             }
         });
     })
@@ -49,4 +58,26 @@ app.post('/blog/new', function(req, res){
     });
 });
 
+app.get('/blog/:id', function(req, res) {
+    articleProvider.findById(req.params.id, function(error, article) {
+        res.render('blog_show.jade',
+        { locals: {
+            title: article.title,
+            article: article
+        }
+        });
+    });
+});
+
+app.post('/blog/addComment', function(req, res) {
+    articleProvider.addCommentToArticle(req.param('_id'), {
+        person: req.param('person'),
+        comment: req.param('comment'),
+        created_at: new Date()
+       } , function( error, docs) {
+           res.redirect('/blog/' + req.param('_id'))
+       });
+});
+
 app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
---
+++ articleprovider-mongodb.js
@@ -0,0 +1,82 @@
+var Db = require('mongodb').Db;
+var Connection = require('mongodb').Connection;
+var Server = require('mongodb').Server;
+var BSON = require('mongodb').BSON;
+var ObjectID = require('mongodb').ObjectID;
+
+ArticleProvider = function(host, port) {
+  this.db= new Db('node-mongo-blog', new Server(host, port, {auto_reconnect: true}, {}));
+  this.db.open(function(){});
+};
+
+
+ArticleProvider.prototype.getCollection= function(callback) {
+  this.db.collection('articles', function(error, article_collection) {
+    if( error ) callback(error);
+    else callback(null, article_collection);
+  });
+};
+
+ArticleProvider.prototype.findAll = function(callback) {
+    this.getCollection(function(error, article_collection) {
+      if( error ) callback(error)
+      else {
+        article_collection.find().toArray(function(error, results) {
+          if( error ) callback(error)
+          else callback(null, results)
+        });
+      }
+    });
+};
+
+
+ArticleProvider.prototype.findById = function(id, callback) {
+    this.getCollection(function(error, article_collection) {
+      if( error ) callback(error)
+      else {
+        article_collection.findOne({_id: article_collection.db.bson_serializer.ObjectID.createFromHexString(id)}, function(error, result) {
+          if( error ) callback(error)
+          else callback(null, result)
+        });
+      }
+    });
+};
+
+ArticleProvider.prototype.save = function(articles, callback) {
+    this.getCollection(function(error, article_collection) {
+      if( error ) callback(error)
+      else {
+        if( typeof(articles.length)=="undefined")
+          articles = [articles];
+        for( var i =0;i< articles.length;i++ ) {
+          article = articles[i];
+          article.created_at = new Date();
+          if( article.comments === undefined ) article.comments = [];
+          for(var j =0;j< article.comments.length; j++) {
+            article.comments[j].created_at = new Date();
+          }
+        }
+
+        article_collection.insert(articles, function() {
+          callback(null, articles);
+        });
+      }
+    });
+};
+
+ArticleProvider.prototype.addCommentToArticle = function(articleId, comment, callback) {
+  this.getCollection(function(error, article_collection) {
+    if( error ) callback( error );
+    else {
+      article_collection.update(
+        {_id: article_collection.db.bson_serializer.ObjectID.createFromHexString(articleId)},
+        {"$push": {comments: comment}},
+        function(error, article){
+          if( error ) callback(error);
+          else callback(null, article)
+        });
+    }
+  });
+};
+
+exports.ArticleProvider = ArticleProvider;
