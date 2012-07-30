f048b8a3de407c7bb141d080bbdf3d96e621b70b
Trucy Phan <trucy.phan@gmail.com>
2012-07-30 16:49:07 -0400

    changed port variable

--- app.js
+++ app.js
@@ -108,5 +108,5 @@ app.post('/blog/addComment', function(req, res) {
        });
 });
 
-app.listen(57266);
+app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
